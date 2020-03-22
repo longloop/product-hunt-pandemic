b6933d5c4a12f499a296245474a58e59ceb81c84
rajan <rbuha@ontic.ai>
2020-03-22 22:59:36 +0530

    fix host

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let config = {
-    mongo_host:'h162s02smovisc7qb11n0u3qeh@ds217360.mlab.com',
+    mongo_host:'ds217360.mlab.com',
     mongo_port:'17360',
     mongo_db:'heroku_7fvcbnzz',
     mongo_user: 'heroku_7fvcbnzz',
