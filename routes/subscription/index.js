8348808514c42c32f0d9bd865df191010f679d97
rajan <rbuha@ontic.ai>
2020-03-22 23:13:19 +0530

    fix

--- routes/subscription/index.js
+++ routes/subscription/index.js
@@ -3,11 +3,10 @@ var router = express.Router();
 
 
 router.post('/', function (req, res, next) {
-    var url = "mongodb://" + CONFIG.mongo_user + ":" + CONFIG.mongo_password + "@" + CONFIG.mongo_host + ":" + CONFIG.mongo_port + "/";
+    var url = "mongodb://" + CONFIG.mongo_user + ":" + CONFIG.mongo_password + "@" + CONFIG.mongo_host + ":" + CONFIG.mongo_port + "/" + CONFIG.mongo_db;
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
-        var dbo = db.db(CONFIG.mongo_db);
-        dbo.collection("subscription").save(req);
+        db.collection("subscription").save(req);
     });
 });
 
