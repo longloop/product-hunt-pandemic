74b8cb72e100c9454588171eadfe4fa0add394cf
rajan <rbuha@ontic.ai>
2020-03-22 23:35:52 +0530

    downgrading

--- routes/home/index.js
+++ routes/home/index.js
@@ -6,10 +6,9 @@ var CONFIG = require('./../../config.js')
 console.log(CONFIG);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const url = "mongodb://" + CONFIG.mongo_user + ":" + CONFIG.mongo_password + "@" + CONFIG.mongo_host + ":" + CONFIG.mongo_port;
-  MongoClient.connect(url, function (err, client) {
+  const url = "mongodb://" + CONFIG.mongo_user + ":" + CONFIG.mongo_password + "@" + CONFIG.mongo_host + ":" + CONFIG.mongo_port + "/" + CONFIG.mongo_db;
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
-    const db = client.db(CONFIG.mongo_db);
     db.collection("offerings").find().limit(5).toArray(function (err, result) {
       if (err) throw err;
       db.close();
