c4bfed23e327801f46bd13fac6668f67d4602015
rajan <rbuha@ontic.ai>
2020-03-22 23:58:18 +0530

    configured mailgun

---
+++ routes/subscription/mailsender.js
@@ -0,0 +1,13 @@
+const mailgun = require("mailgun-js");
+const DOMAIN = 'YOUR_DOMAIN_NAME';
+const  api_key= ""
+const mg = mailgun({apiKey: api_key, domain: DOMAIN});
+const data = {
+    from: 'Excited User <me@samples.mailgun.org>',
+    to: 'bar@example.com, YOU@YOUR_DOMAIN_NAME',
+    subject: 'Hello',
+    text: 'Testing some Mailgun awesomness!'
+};
+mg.messages().send(data, function (error, body) {
+    console.log(body);
+});
