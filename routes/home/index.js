241d83cbbd4142bf9207ab70ee1e13d7bb6764c0
rajan <rbuha@ontic.ai>
2020-03-22 23:08:46 +0530

    fix

--- routes/home/index.js
+++ routes/home/index.js
@@ -2,13 +2,12 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 
-const CONFIG = require('./../../config.js')
-const MONGO_URL = "mongodb://" + CONFIG.mongo_user + ":" + CONFIG.mongo_password + "@" + CONFIG.mongo_host + ":" + CONFIG.mongo_port + "/";
-
+var CONFIG = require('./../../config.js')
 console.log(CONFIG);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  MongoClient.connect(MONGO_URL, function (err, db) {
+  var url = "mongodb://" + CONFIG.mongo_user + ":" + CONFIG.mongo_password + "@" + CONFIG.mongo_host + ":" + CONFIG.mongo_port + "/";
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db(CONFIG.mongo_db);
     dbo.collection("offerings").find().limit(5).toArray(function(err, result) {
--- routes/subscription/index.js
+++ routes/subscription/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
-
-const CONFIG = require('./../../config.js')
-const MONGO_URL = "mongodb://" + CONFIG.mongo_user + ":" + CONFIG.mongo_password + "@" + CONFIG.mongo_host + ":" + CONFIG.mongo_port + "/";
 
 
 router.post('/', function (req, res, next) {
-    MongoClient.connect(MONGO_URL, function (err, db) {
+    var url = "mongodb://" + CONFIG.mongo_user + ":" + CONFIG.mongo_password + "@" + CONFIG.mongo_host + ":" + CONFIG.mongo_port + "/";
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db(CONFIG.mongo_db);
         dbo.collection("subscription").save(req);
