eaf33914c101889e4cabac5f68e63b40553838a9
Sumit Sinha <sumit.s@browserstack.com>
2020-03-22 13:05:41 +0530

    added mongo connection

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongojs = require('mongojs');
+
+// setup db
+var db = mongojs('productHuntPandemic');
+//var offerings = db.collection('offerings');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
