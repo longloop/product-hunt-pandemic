0b850203dee7b0996e58ff1a4f0ecda4b47928cb
rajan <rbuha@ontic.ai>
2020-03-22 22:45:19 +0530

    mongo urls

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {
-    mongo_host:'localhost',
-    mongo_port:'27017',
-    mongo_db:'co_victory',
+    mongo_host:'h162s02smovisc7qb11n0u3qeh@ds217360.mlab.com',
+    mongo_port:'17360',
+    mongo_db:'heroku_7fvcbnzz',
 }
 
 module.exports = config;
